8e45d53d8cf589a961d3b93fb7580b18c697dd1a
vpaliyX <vpaliy97@gmail.com>
2018-02-16 00:16:05 -0600

    Update factorial

--- factorial.asm
+++ factorial.asm
@@ -1,48 +1,46 @@
 section .data
-  x:      dq 0x0
-  scan:   db '%d',0x0
-  print:  db 'fact(%d) = %d',0xA
+  n:                 dq 0x0
+  print_format:      db '%d', 0x0
+  scan_format:       db 'factorial(%d) = %d',0xA
 
 section .text
   bits 64
-  global _main
-  global factorial
-  extern _scanf
   extern _printf
+  extern _scanf
+  global _main
+  global _factorial
 
 _main:
 default rel
   push rbp
   mov rbp, rsp
-  lea rdi, [scan]
-  lea rsi, [x]
+  lea rdi, [print_format]
+  lea rsi, [n]
   xor rax, rax
   call _scanf
-  mov rdi, [x]
+  mov rdi, [n]
   call _factorial
-  lea rdi, [print]
-  mov rsi, [x]
+  lea rdi, [scan_format]
+  mov rsi, [n]
   mov rdx, rax
-  xor rax, rax
   call _printf
   xor rax, rax
   leave
   ret
 
-
 _factorial:
   push rbp
   mov rbp, rsp
-  sub rsp, 0x10
   cmp rdi, 0x1
-  jg _greater
+  jg .greater
   mov rax, 0x1
   leave
   ret
-_greater:
-  mov [rsp+0x8], rdi
+.greater:
+  push rdi
   dec rdi
   call _factorial
-  imul rax, [rsp+0x8]
+  imul rax, [rsp]
+  mov rsp, rbp
   leave
   ret
