b6261dae652b0e203b321921b021c101d9e458b1
vpaliyX <vpaliy97@gmail.com>
2018-02-17 22:06:06 -0600

    Add min/max

--- parentheses.asm
+++ parentheses.asm
@@ -13,13 +13,32 @@ default rel
   mov rbp, rsp
   mov rax, qword array
   push rax
-  push 0x64
+  push 0x63
   call _fill
   add rsp, 0x10
   mov rax, qword array
   push rax
-  push 0x10
+  push 0x64
   call _print
   add rsp, 0x10
+  mov rsi, qword array
+  mov rdi, 0x64
+  call _min
+  ;
+  lea rdi, [format]
+  mov rsi, rax
+  xor rax, rax
+  call _printf
+  xor rax, rax
+
+  mov rsi, qword array
+  mov rdi, 0x64
+  call _max
+  ;
+  lea rdi, [format]
+  mov rsi, rax
+  xor rax, rax
+  call _printf
+  xor rax, rax
   leave
   ret
