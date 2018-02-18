9d7e8f0fc7b3edd2a3ce3d24cd46c989a7ce44c8
vpaliyX <vpaliy97@gmail.com>
2018-02-18 15:58:15 -0600

    Finish bubble sort

--- sorting/bubble.asm
+++ sorting/bubble.asm
@@ -1,4 +1,45 @@
+%include '../utils/arrays.inc'
 
+section .bss
+  array resq 0xA
+
+section .text
+  global _main
+  global _bubble
+
+_main:
+default rel
+  push rbp
+  mov rbp, rsp
+
+  mov rax, qword array
+  push rax
+  push 0xA
+  call _fill
+  add rsp, 0x10
+
+  mov rax, qword array
+  push rax
+  push 0xA
+  call _print
+  add rsp, 0x10
+
+  mov rax, qword array
+  push rax
+  push 0xA
+  call _bubble
+  add rsp, 0x10
+
+  mov rax, qword array
+  push rax
+  push 0xA
+  call _print
+  add rsp, 0x10
+
+  leave
+  ret
+
+; Bubble sort (2 loops) O(n)
 ; 1 - array
 ; 2 - length
 _bubble:
@@ -8,6 +49,7 @@ _bubble:
   push rdi
   mov rsi, [rbp + 0x18] ; array
   mov rdi, [rbp + 0x10] ; length
+  lea rdi, [rsi + 0x8 * rdi]
 .while:
   cmp rsi, rdi
   jge .end
@@ -18,7 +60,16 @@ _bubble:
   jge .reset
   push rax
   mov rax, [rax]
-  cmp [rsi], rax
+  cmp rax, [rsi]
+  jge .proceed
+  xor rax, [rsi]
+  xor [rsi], rax
+  xor rax, [rsi]
+.proceed:
+  pop r8
+  mov [r8], rax
+  mov rax, r8
+  lea rax, [rax + 0x8]
   jmp .inner
 .reset:
   lea rsi, [rsi + 0x8]
