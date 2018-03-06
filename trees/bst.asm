347c3dabfb98f19688332be9f7c9f5708270ea98
vpaliyX <vpaliy97@gmail.com>
2018-03-05 18:36:24 -0600

    Update bst.asm

--- trees/bst.asm
+++ trees/bst.asm
@@ -1,155 +1,194 @@
+; Created by Vasyl Paliy
 
 %define null 0x00000000
 
-section .data
-  struc TreeNode
-    .key:     resq 1
-    .info:    resq 1
-    .left:    resq 1
-    .right:   resq 1
-    .parent:  resq 1
-  endstruc
+; 1 - root node
+; 2 - key
+; 3 - value
+%macro insert 3
+  mov rdi, %2
+  mov rsi, %3
+  mov rdx, %1
+  call _insert
+%endmacro
 
-  format:   db '%d',0xA,0x0
+; 1 - root node
+; 2 - search key
+%macro find 2
+  mov rdi, %1
+  mov rsi, %2
+  call _find
+%endmacro
+
+; 1 - key
+; 2 - value
+%macro create 2
+  mov rdi, %1
+  mov rsi, %2
+  call _create
+%endmacro
+
+%macro show 1
+  mov rdi, %1
+  call _show
+%endmacro
+
+section .data
+  struc node
+    .key:       resq 1
+    .info:      resq 1
+    .left:      resq 1
+    .right:     resq 1
+    .parent:    resq 1
+  endstruc
+  format:       db '%d',0xA,0x0
+
 section .text
+  bits 64
   global _main
-  global _create
   global _insert
+  global _new
   extern _malloc
   extern _printf
 
+;  16, 8, 7, 10, 18, 17, 19
 _main:
   push rbp
   mov rbp, rsp
 
-  call _create
-  mov qword [rax + TreeNode.key], 0x18
-  mov qword [rax + TreeNode.info], 0x10
-
-  mov rdi, rax
-  mov rsi, 0x24
-  mov rdx, 0x10
-  call _insert
-
-  mov rdi, rax
-  mov rsi, 0x19
-  mov rdx, 0x10
-  call _insert
-
-  mov rdi, rax
-  mov rsi, 0x17
-  mov rdx, 0x10
-  call _insert
-
-  mov rdi, rax
-  mov rsi, 0x16
-  mov rdx, 0x10
-  call _insert
-
-  mov rdi, rax
-  mov rsi, 0x15
-  mov rdx, 0x10
-  call _insert
-
-  mov rdi, rax
-  mov rsi, 0x20
-  mov rdx, 0x10
-  call _insert
-
-  mov rdi, rax
-  call _show
+  create 0x10, 0x10
+  insert rax, 0x8, 0x10
+  insert rax , 0x7, 0x10
+  insert rax, 0xA, 0x10
+  insert rax, 0x12, 0x10
+  insert rax, 0x11, 0x10
+  insert rax, 0x13, 0x10
+  show rax
 
   leave
   ret
 
+; no parameters
+; return: rax - new empty node
+_new:
+  push rbp
+  mov rbp, rsp
+  mov rdi, node_size
+  call _malloc
+  mov rdi, null
+  mov [rax + node.key], rdi
+  mov [rax + node.info], rdi
+  mov [rax + node.left], rdi
+  mov [rax + node.right], rdi
+  mov [rax + node.parent], rdi
+  leave
+  ret
+
+; rdi - key
+; rsi - info
 _create:
   push rbp
   mov rbp, rsp
-  mov rdi, TreeNode_size
-  xor rax, rax
-  call _malloc
-  mov rdi, null
-  ; assign null to each field
-  mov [rax + TreeNode.key], rdi
-  mov [rax + TreeNode.info], rdi
-  mov [rax + TreeNode.left], rdi
-  mov [rax + TreeNode.right], rdi
-  mov [rax + TreeNode.parent], rdi
+  push rdi
+  push rsi
+  call _new
+  pop rsi
+  pop rdi
+  mov [rax + node.key], rdi
+  mov [rax + node.info], rsi
   leave
   ret
 
-; rdi - root node
+; rdi - node
 ; rsi - key
-; rdx - info
+; return: rax - node that contains the key
+_find:
+  push rbp
+  mov rbp, rsp
+  mov rax, null
+.while:
+  cmp rdi, null
+  je .done
+  cmp [rdi + node.key], rsi
+  jg .goleft
+  jl .goright
+  mov rax, rdi
+  jmp .done
+.goleft:
+  mov rdi, [rdi + node.left]
+  jmp .while
+.goright:
+  mov rdi, [rdi + node.right]
+  jmp .while
+.done:
+  leave
+  ret
+
+; rdi - key
+; rsi - info
+; rdx - node
 ; return: rax - root node
 _insert:
   push rbp
   mov rbp, rsp
-  push rdi
   push rdx
-  push rsi
-  push rdi
+  push rdx
+  mov [rsp], rdx
   call _create
-  pop rdi
-  pop rsi
   pop rdx
-  mov r8, rdi
+  mov r8, rdx
 .for:
-  cmp rdi, null
-  je .leaf
-  mov r8, rdi
-  cmp [r8 + TreeNode.key], rsi
-  jmp .lower
-  mov rdi, [rdi + TreeNode.right]
+  cmp rdx, null
+  je .insert
+  mov r8, rdx
+  cmp [rdx + node.key], rdi
+  jle .goright
+  jg  .goleft
+.goleft:
+  mov rdx, [rdx + node.left]
   jmp .for
-.lower:
-  mov rdi, [rdi + TreeNode.left]
+.goright:
+  mov rdx, [rdx + node.right]
   jmp .for
-.leaf:
-  mov [rax + TreeNode.parent], r8
-  mov [rax + TreeNode.key], rsi
-  mov [rax + TreeNode.info], rdx
-  cmp [r8 + TreeNode.key], rsi
-  jge .left
-  mov [r8 + TreeNode.right], rax
-  jmp .done
+.insert:
+  mov [rax + node.parent], r8
+  cmp [r8 + node.key], rdi
+  jle .right
+  je  .left
 .left:
-  mov [r8 + TreeNode.left], rax
-.done:
+  mov [r8 + node.left], rax
+  jmp .end
+.right:
+  mov [r8 + node.right], rax
+.end:
   pop rax
   leave
   ret
 
-; 24, 23, 22, 21, 25, 24, 32
 ; rdi - root node
 _show:
   push rbp
   mov rbp, rsp
-  ; if null is reached, just leave
   cmp rdi, null
-  je .done
-  ; save rdi in stack
+  je .end
   sub rsp, 0x10
   mov [rsp], rdi
 
-  ; print its key
-  mov rsi, [rdi + TreeNode.key]
+  mov rsi, [rdi + node.key]
   mov rdi, qword format
-  xor rax, rax
   call _printf
 
-  ; show its left node
+  ; show left subtree
   mov rdi, [rsp]
-  mov rdi, [rdi + TreeNode.left]
+  mov rdi, [rdi + node.left]
   call _show
 
-  ; show its right node
+  ; show right subtree
   mov rdi, [rsp]
-  mov rdi, [rdi + TreeNode.right]
+  mov rdi, [rdi + node.right]
   call _show
 
-  ; release stack
   add rsp, 0x10
-.done:
+.end:
   leave
   ret
